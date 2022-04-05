9a7683a0f3ebf0b928c549ce4b601bf026de55ff
Remix Workspace <>
2022-04-06 01:50:30 +0200

    data structures

--- marketplace.sol
+++ marketplace.sol
@@ -6,4 +6,19 @@ contract marketplace {
     constructor() {
         owner = msg.sender;
     }
+
+    mapping(address => bool) registeredPublishers;
+    mapping(address => string) publisherName;
+    mapping(address => uint256) publisherScore;
+
+    struct offer {
+        address publisher;
+        string metadata;
+        mapping(address => bool) boughtAccess;
+        uint256[] reviews;
+        uint256 price;
+        string sample;
+        string data;
+        string[] dataArray;
+    }
 }
